7827f8239f4d0a9cc95b354bf185c9787129590a
Kimaaki <drmngeneral@gmail.com>
2025-10-17 14:23:55 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,9 @@ import { Label } from '@/components/ui/label'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Textarea } from '@/components/ui/textarea'
 import { toast } from 'sonner'
+
+import { supabase } from "@/lib/supabase-browser";
+
 import { 
   Users, 
   TrendingUp, 
